52610929e69f3ab2bf355292318b763ef6c8f982
Sean Milbert H. Pinote <pinotesean6@gmail.com>
2024-11-06 19:44:41 +0800

    Update Auditable.java

--- RobinlandPos/src/main/java/com/robinland/pos/RobinlandPos/model/Auditable.java
+++ RobinlandPos/src/main/java/com/robinland/pos/RobinlandPos/model/Auditable.java
@@ -1,9 +1,5 @@
 package com.robinland.pos.RobinlandPos.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedBy;
@@ -12,6 +8,9 @@ import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
