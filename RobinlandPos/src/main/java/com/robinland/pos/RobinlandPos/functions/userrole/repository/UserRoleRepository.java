206ce9675dc28b7d2dfe55558a557c5caf4b0c49
Sean Milbert H. Pinote <pinotesean6@gmail.com>
2024-11-06 23:59:35 +0800

    Create UserRoleRepository.java

---
+++ RobinlandPos/src/main/java/com/robinland/pos/RobinlandPos/functions/userrole/repository/UserRoleRepository.java
@@ -0,0 +1,7 @@
+package com.robinland.pos.RobinlandPos.functions.userrole.repository;
+
+import com.robinland.pos.RobinlandPos.model.UserRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
+}
