b434ffbf5acbd3b9e640aabb01a3e5dc2685177c
Sean Milbert H. Pinote <pinotesean6@gmail.com>
2024-11-06 23:59:07 +0800

    Update UserService.java

--- RobinlandPos/src/main/java/com/robinland/pos/RobinlandPos/functions/user/service/UserService.java
+++ RobinlandPos/src/main/java/com/robinland/pos/RobinlandPos/functions/user/service/UserService.java
@@ -1,8 +1,11 @@
 package com.robinland.pos.RobinlandPos.functions.user.service;
 
 import com.robinland.pos.RobinlandPos.functions.user.dto.UserRequestDto;
+import com.robinland.pos.RobinlandPos.functions.user.dto.UserResponseDto;
 import com.robinland.pos.RobinlandPos.functions.user.repository.UserRepository;
+import com.robinland.pos.RobinlandPos.model.Role;
 import com.robinland.pos.RobinlandPos.model.User;
+import com.robinland.pos.RobinlandPos.model.UserRole;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,8 +13,10 @@ import org.springframework.util.ObjectUtils;
 
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserServiceInterface{
@@ -25,19 +30,27 @@ public class UserService implements UserServiceInterface{
 
     @Override
     @Transactional(rollbackOn = Exception.class)
-    public Optional<User> getUser(Long userId) throws Exception{
+    public UserResponseDto getUser(Long userId) throws Exception{
+        UserResponseDto responseDto = new UserResponseDto();
         Optional<User> existingUser = userRepository.findById(userId);
 
-        if(ObjectUtils.isEmpty(existingUser)){
+        if(ObjectUtils.isEmpty(existingUser)) {
             throw new Exception("No user found");
         }
 
-        return existingUser;
+        existingUser.ifPresent(user -> {
+            BeanUtils.copyProperties(user, responseDto);
+
+            responseDto.setUserRoles(extractRolesFromUserRoles(user));
+        });
+
+        return responseDto;
     }
 
     @Override
     @Transactional(rollbackOn = Exception.class)
-    public User addUser(UserRequestDto requestDto) throws Exception {
+    public UserResponseDto addUser(UserRequestDto requestDto) throws Exception {
+        UserResponseDto responseDto = new UserResponseDto();
         User user = new User();
         LocalDateTime dateTimeNow = LocalDateTime.now();
         BeanUtils.copyProperties(requestDto, user);
@@ -51,16 +64,18 @@ public class UserService implements UserServiceInterface{
 
         try {
             user = userRepository.save(user);
+            BeanUtils.copyProperties(user, responseDto);
         } catch (Exception e) {
             throw new Exception("Failed to add user data");
         }
 
-        return user;
+        return responseDto;
     }
 
     @Override
     @Transactional(rollbackOn = Exception.class)
-    public User updateUser(Long id, UserRequestDto requestDto) throws Exception {
+    public UserResponseDto updateUser(Long id, UserRequestDto requestDto) throws Exception {
+        UserResponseDto responseDto = new UserResponseDto();
         User oldUser = new User();
         User updatedUser = new User();
         Optional<User> existingUser = userRepository.findById(id);
@@ -77,10 +92,15 @@ public class UserService implements UserServiceInterface{
         if (isEmailUnique(updatedUser.getEmail())) {
             throw new Exception("Email is already taken.");
         }
+        try {
+            updatedUser = userRepository.save(updatedUser);
+            BeanUtils.copyProperties(updatedUser, responseDto);
+        }catch(Exception e){
+            throw new Exception("Failed to update user data");
+        }
 
-        updatedUser = userRepository.save(updatedUser);
 
-        return updatedUser;
+        return responseDto;
     }
 
     @Override
@@ -97,5 +117,11 @@ public class UserService implements UserServiceInterface{
 
     private boolean isEmailUnique(String email) {
         return !ObjectUtils.isEmpty(userRepository.findByEmail(email));
+    }
+
+    private List<Role> extractRolesFromUserRoles(User user) {
+        return user.getRoles().stream()
+                .map(UserRole::getRole)
+                .collect(Collectors.toList());
     }
 }
