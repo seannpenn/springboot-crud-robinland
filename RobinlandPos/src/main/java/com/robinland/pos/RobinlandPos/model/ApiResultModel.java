b18de88995888a9d8430deff9fa325eaf9ebadb9
Sean Milbert H. Pinote <pinotesean6@gmail.com>
2024-11-06 19:44:35 +0800

    Update ApiResultModel.java

--- RobinlandPos/src/main/java/com/robinland/pos/RobinlandPos/model/ApiResultModel.java
+++ RobinlandPos/src/main/java/com/robinland/pos/RobinlandPos/model/ApiResultModel.java
@@ -1,24 +1,18 @@
 package com.robinland.pos.RobinlandPos.model;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Setter
 @Getter
 @Builder
 public class ApiResultModel {
     private Boolean isSuccess;
     private String message;
-    private Object messageParams;
     private Object resultData;
-    private List<String> errorMessages;
-    private Object errorCodes;
-    private String exceptionType;
     private Integer length;
 }
