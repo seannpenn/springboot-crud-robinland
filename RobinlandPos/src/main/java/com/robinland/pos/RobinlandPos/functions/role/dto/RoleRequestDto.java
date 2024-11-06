754a9651fbb632f2a716e67ba7a156caaead8ab4
Sean Milbert H. Pinote <pinotesean6@gmail.com>
2024-11-06 21:18:29 +0800

    Create RoleRequestDto.java

---
+++ RobinlandPos/src/main/java/com/robinland/pos/RobinlandPos/functions/role/dto/RoleRequestDto.java
@@ -0,0 +1,14 @@
+package com.robinland.pos.RobinlandPos.functions.role.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class RoleRequestDto {
+    private String name;
+}
