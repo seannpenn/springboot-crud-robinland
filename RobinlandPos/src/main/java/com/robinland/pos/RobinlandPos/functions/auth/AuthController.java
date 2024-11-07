e822391efd58133f4408ec53ce1c3490910a72ea
Sean Milbert H. Pinote <pinotesean6@gmail.com>
2024-11-08 00:15:36 +0800

    add login endpoint for authcontroller

--- RobinlandPos/src/main/java/com/robinland/pos/RobinlandPos/functions/auth/AuthController.java
+++ RobinlandPos/src/main/java/com/robinland/pos/RobinlandPos/functions/auth/AuthController.java
@@ -1,5 +1,6 @@
 package com.robinland.pos.RobinlandPos.functions.auth;
 
+import com.robinland.pos.RobinlandPos.dto.LoginDto;
 import com.robinland.pos.RobinlandPos.dto.RegisterDto;
 import com.robinland.pos.RobinlandPos.functions.role.repository.RoleRepository;
 import com.robinland.pos.RobinlandPos.functions.user.repository.UserRepository;
@@ -11,13 +12,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("auth")
@@ -36,6 +43,33 @@ public class AuthController {
         this.roleRepository = roleRepository;
         this.userRoleRepository = userRoleRepository;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("login")
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto){
+        try {
+            Optional<User> user = userRepository.findByUserName(loginDto.getUserName());
+
+            if(ObjectUtils.isEmpty(user)){
+                throw new Exception("Username doesn't exist");
+            }else{
+                Authentication authentication = authenticationManager.authenticate(
+                        new UsernamePasswordAuthenticationToken(
+                                loginDto.getUserName(),
+                                loginDto.getPassword()));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            }
+
+            return new ResponseEntity<>("User signed in successfully!", HttpStatus.OK);
+        } catch (AuthenticationException e) {
+            System.out.println("AuthenticationException " + e.getMessage());
+
+            return new ResponseEntity<>("Password is incorrect!", HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
     }
 
     @PostMapping("register")
